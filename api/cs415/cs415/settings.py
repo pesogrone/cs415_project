0d78e2b6d3fb40e737a161c24d0aba4aa0698a80
Ronald Britos <rmbgkpo@ensign.edu>
2024-03-30 18:33:34 -0600

    all ips

--- api/cs415/cs415/settings.py
+++ api/cs415/cs415/settings.py
@@ -76,14 +76,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cs415.wsgi.application'
 
-CORS_ALLOWED_ALL_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:8080",
-    "http://localhost:8000",
-    "http://127.0.0.1.8000",
-    "http://44.214.75.250",
-    "http://44.221.42.102"
-]
+CORS_ALLOWED_ALL_ORIGINS = ['*']
+    # "http://localhost:3000",
+    # "http://localhost:8080",
+    # "http://localhost:8000",
+    # "http://127.0.0.1.8000",
+    # "http://44.214.75.250",
+    # "http://44.221.42.102"
+# ]
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
